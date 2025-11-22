ad87f1ad5985947814933d0fab4ed53a02b0a472
ccz82 <chunzhen82@gmail.com>
2025-11-22 23:30:57 +0800

    sig

--- home/lan-mouse.nix
+++ home/lan-mouse.nix
@@ -9,9 +9,14 @@
   programs.lan-mouse = {
     enable = true;
     settings = {
-      # authorized_fingerprints = {
-      #   "3b:65:ef:86:d8:43:88:6a:65:ee:17:30:42:ef:6d:c9:69:76:31:2d:56:58:02:7e:a2:b3:cc:7c:e9:9f:45:a4" = "ccz-desktop";
-      # };
+      authorized_fingerprints = lib.mkMerge [
+        (lib.mkIf (hostname == "zenbook") {
+          "14:c9:eb:5a:f9:08:46:4a:29:c0:35:24:95:1b:54:59:60:d2:cb:14:6c:30:80:22:72:37:9c:68:92:42:df:10" = "ccz-desktop";
+        })
+        (lib.mkIf (hostname == "ccz-desktop") {
+          "03:dd:c1:92:fb:8e:8e:72:40:36:b5:2a:12:ab:73:0b:ad:3a:14:86:b3:b0:1a:8a:ae:05:04:fb:2d:f9:a8:2f" = "zenbook";
+        })
+      ];
       clients = [
         (lib.mkIf
           (hostname == "zenbook")
